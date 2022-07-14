50b9c1f939f87f754684539be98aaac7db5c470f
Fitore <ttahirifitore@gmail.com>
2022-07-15 00:11:02 +0200

    Revert "Merge branch 'basecode' of https://github.com/fitoretahiri/Food-delivery-app-LabCouse1 into basecode"
    This reverts commit bef946afacdd5609e882cf44ab0bf9a9b9d444ca, reversing
    changes made to a0e6111ca97e6dcffcf40c3b7404447c21c063bf.

---
+++ Migrations/20220712222339_updateTables.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Food_delivery_app_LabCouse1.Migrations
+{
+    public partial class updateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Porosia",
+                columns: table => new
+                {
+                    PorosiaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataPorosise = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Adresa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Statusi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Porosia", x => x.PorosiaId);
+                    table.ForeignKey(
+                        name: "FK_Porosia_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    PorosiaId = table.Column<int>(type: "int", nullable: false),
+                    MenuId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cart_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cart_Menu_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menu",
+                        principalColumn: "MenuID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cart_Porosia_PorosiaId",
+                        column: x => x.PorosiaId,
+                        principalTable: "Porosia",
+                        principalColumn: "PorosiaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagesa",
+                columns: table => new
+                {
+                    PagesaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CmimiTotal = table.Column<double>(type: "float", nullable: false),
+                    PorosiaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagesa", x => x.PagesaId);
+                    table.ForeignKey(
+                        name: "FK_Pagesa_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pagesa_Porosia_PorosiaId",
+                        column: x => x.PorosiaId,
+                        principalTable: "Porosia",
+                        principalColumn: "PorosiaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_MenuId",
+                table: "Cart",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_PorosiaId",
+                table: "Cart",
+                column: "PorosiaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_UserId",
+                table: "Cart",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagesa_PorosiaId",
+                table: "Pagesa",
+                column: "PorosiaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagesa_UserId",
+                table: "Pagesa",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Porosia_UserId",
+                table: "Porosia",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cart");
+
+            migrationBuilder.DropTable(
+                name: "Pagesa");
+
+            migrationBuilder.DropTable(
+                name: "Porosia");
+        }
+    }
+}
